f48f7994b257f39624c98d1b82dc992a7f46497a
Spencer Weishaar <sbweishaar@icloud.com>
2025-03-02 17:47:22 -0600

    Started adding data to tourney_dict. The dictionary is kind of messed up right now, so that will need to be fixed.

--- dataFetcher.py
+++ dataFetcher.py
@@ -19,12 +19,12 @@ if __name__ == "__main__":
 
         start_year = 2004  # First tournament year we are checking 
         year = start_year   # year variable that increments through the for loop
-        end_year = 2024      # Last tournament year we are checking
+        end_year = 2023      # Last tournament year we are checking
         year_len = end_year - start_year   # length of time we are checking
 
         tourney_dict = dict() # dictionary that holds all the data from the ncaa tourney
 
-        for i in range(4, 24):  # for loop that looks through the ncaa tourneys
+        for i in range(4, 5):  # for loop that looks through the ncaa tourneys
 
             if year == 2020:  # Skips 2020 year, becuase it was canceled because of covid
                 year += 1
@@ -36,26 +36,62 @@ if __name__ == "__main__":
 
             tourney_dict[year_str] = dict() # creates a new key and value pair in the ncaa tourney results dictionary
 
+            year_dict = tourney_dict[year_str]
 
             with open(file_path, 'r') as tourney_stats:   # Opens csv file
                 tourney_data = csv.reader(tourney_stats, delimiter=',')    # assigns csv to a variable
                 next(tourney_data) # Skips the header
-                for row in tourney_data:   # iterates through every row in the ncaa tourney table for that year
-                    game = row
-                    print(f"{year_str} row = {game}, data type = {type(game)}")  # prints every row in the table
+                for game in tourney_data:   # iterates through every row in the ncaa tourney table for that year
 
-                    rounds = 0
-                    for round in range(rounds):
-                        print("in loop")
-                        # This loop will go through the teams and assign them to the master list, along with how many wins they had in the tourney, scores, opponents, etc.
-                        # This is where we will enter the info into the dictionary like the one in the image on discord, unless there is a better way to do it. 
+                    # print(f"{year_str} row = {game}, data type = {type(game)}")  # prints every row in the table
 
+                    game_string = game[2]   # assigns game_string variable to the string describing the two teams ()
+
+
+                    game_list = game_string.split(" ")  # Converts the game_string to a list
+
+                    round = game[1]  # round the current game is in
+
+                    team_seed = game_list[0][1]   # seed of team 
+                    team = game_list[1]   # team name
+                    opp_seed = game_list[3][1]    # seed of opponent
+                    opp_team = " ".join(game_list[4:])    # oppenents team name
+                    score = game[3] # score of the game
+
+                    game_stats = [team, opp_team, score] # list that (for now, might add seeds and win variables) holds teams and score of the game
+
+                    year_dict[f"{team}"] = dict()
+
+                    team_dict = year_dict
+
+                    team_dict[f"{round}"] = []
+                    round_list = team_dict[f"{round}"]
+                    round_list.extend(game_stats)
+
+
+
+
+
+
+
+                   
+
+                    # tourney_year[""]
+
+
+                    
 
             year += 1
 
         return tourney_dict
     
-    get_tourney_info()
+
+    def main():
+        
+        tourney_dict = get_tourney_info()
+
+        print(tourney_dict)
+
 
 
 
@@ -63,7 +99,7 @@ if __name__ == "__main__":
     
 
 
-
+main()
 
 
 
